19d48a6d48584a3302388e46a78ff83766b49857
Nickollee <39786643+Nickollee@users.noreply.github.com>
2019-04-11 15:33:45 +0800

    Update assignment2.py

--- week01/assignment2.py
+++ week01/assignment2.py
@@ -6,17 +6,54 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # 读入显示
-img = cv2.imread('D:\learning_materials\Second_grade\AI_for_NLP&CV_Train\courseware\Week1\\1.jpg')
-#BGR转YUV显示
-img_yuv = cv2.cvtColor(img,cv2.COLOR_BGR2YUV)
-#分离
-Y,U,V = cv2.split(img_yuv)
-#随机变化颜色？？
-Y = Y+np.random.rand(Y.shape)
-#通道合并
-img_yuv = cv2.merge([Y,U,V])
-#YUV转回BGR空间
-img_yuv = cv2.cvtColor(img_yuv,cv2.COLOUR_BGR2YUV)
+img = cv2.imread('D:\learning_materials\Second_grade\AI_for_NLP&CV_Train\courseware\CV\week2\\flower.png')
+
+def random_yuv_color(img):
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    Y, U, V = cv2.split(img_yuv)
+
+    y_rand = random.randint(-50, 50)
+    if y_rand == 0:
+        pass
+    elif y_rand > 0:
+        lim = 255 - y_rand
+        Y[Y > lim] = 255
+        Y[Y <= lim] = (y_rand + Y[Y <= lim]).astype(img_yuv.dtype)
+    elif y_rand < 0:
+        lim = 0 < y_rand
+        Y[Y < lim] = 0
+        Y[Y >= lim] = (y_rand + Y[Y >= lim]).astype(img_yuv.dtype)
+
+    u_rand = random.randint(-50, 50)
+    if u_rand == 0:
+        pass
+    elif u_rand > 0:
+        lim = 255 - u_rand
+        U[U > lim] = 255
+        U[U <= lim] = (u_rand + U[U <= lim]).astype(img_yuv.dtype)
+    elif u_rand < 0:
+        lim = 0 < u_rand
+        U[U < lim] = 0
+        U[U >= lim] = (u_rand + U[U >= lim]).astype(img_yuv.dtype)
+    v_rand = random.randint(-50, 50)
+    if v_rand == 0:
+        pass
+    elif v_rand > 0:
+        lim = 255 - v_rand
+        V[V > lim] = 255
+        V[V <= lim] = (v_rand + V[V <= lim]).astype(img_yuv.dtype)
+    elif v_rand < 0:
+        lim = 0 < v_rand
+        V[V < lim] = 0
+        V[V >= lim] = (v_rand + V[V >= lim]).astype(img_yuv.dtype)
+
+    img_merge = cv2.merge((Y, U, V))
+    return cv2.cvtColor(img_merge, cv2.COLOR_YUV2BGR)
 
 
-cv2.imshow('img_yuv', img_yuv)
+
+img_random_yuv = random_yuv_color(img)
+cv2.imshow('img_random_yuv', img_random_yuv)
+key = cv2.waitKey()
+if key == 27:
+    cv2.destroyAllWindows()
